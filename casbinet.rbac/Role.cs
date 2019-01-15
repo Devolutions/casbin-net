2df0ff58939e49271987fad4e7549b4bfdb1c20f
Jonathan Trepanier <jtrepanier@devolutions.net>
2019-01-15 13:32:17 -0500

    Casbinet initialization

---
+++ casbinet.rbac/Role.cs
@@ -0,0 +1,24 @@
+﻿namespace casbinet.rbac
+{
+    using System.Collections.Generic;
+
+    public class Role
+    {
+        private string name;
+
+        private List<Role> roles = new List<Role>();
+
+        public Role(string name)
+        {
+            this.name = name;
+        }
+
+        public void AddRole(Role role)
+        {
+            foreach (Role role in roles)
+            {
+                
+            }
+        }
+    }
+}
---
+++ casbinet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace casbinet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
