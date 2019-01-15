1e85ab642a6ecc162df61481ac1933fbe2238aae
Eugene Richer <ebernard@devolutions.net>
2019-01-15 16:51:16 -0500

    persist continuation

--- casbinet/Persist/Helper.cs
+++ casbinet/Persist/Helper.cs
@@ -1,17 +1,36 @@
 ﻿namespace casbinet.persist
 {
+    using System;
     using System.Collections.Generic;
     using System.Threading;
 
-    //import org.casbin.jcasbin.model.Model;
-
-    public interface Watcher
+    public class Helper
     {
-        void setUpdateCallback(Thread runnable); 
+        public interface loadPolicyLineHandler<T, U>
+        {
+            void accept(T t, U u);
+        }
 
-        void update();
+        public static void loadPolicyLine(String line, Model model)
+        {
+            if (line == "")
+            {
+                return;
+            }
 
+            if (line[0] == '#')
+            {
+                return;
+            }
+
+            String[] tokens = line.Split(", ");
+
+            String key = tokens[0];
+            String sec = key.Substring(0, 1);
+            model.model.get(sec).get(key).policy.add(Arrays.asList(Arrays.copyOfRange(tokens, 1, tokens.length)));
+        }
     }
 
-    
+
+
 }
