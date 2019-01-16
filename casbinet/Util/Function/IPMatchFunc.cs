cc88a3c21f92f06f4c7a71f4d2ff430070dec3f2
Eugene Richer <ebernard@devolutions.net>
2019-01-16 10:52:17 -0500

    Util/Functions

---
+++ casbinet/Util/Function/IPMatchFunc.cs
@@ -0,0 +1,38 @@
+﻿namespace casbinet.util.function
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    //using AbstractFunction = com.googlecode.aviator.runtime.function.AbstractFunction;
+    //using FunctionUtils = com.googlecode.aviator.runtime.function.FunctionUtils;
+    //using AviatorBoolean = com.googlecode.aviator.runtime.type.AviatorBoolean;
+    //using AviatorObject = com.googlecode.aviator.runtime.type.AviatorObject;
+
+    public class Helper
+    {
+        public interface loadPolicyLineHandler<T, U>
+        {
+            void accept(T t, U u);
+        }
+
+        public class IPMatchFunc : AbstractFunction
+        {
+            public virtual AviatorObject call(IDictionary<string, object> env, AviatorObject arg1, AviatorObject arg2)
+            {
+                string ip1 = FunctionUtils.getStringValue(arg1, env);
+                string ip2 = FunctionUtils.getStringValue(arg2, env);
+
+                return AviatorBoolean.valueOf(BuiltInFunctions.ipMatch(ip1, ip2));
+            }
+
+            public virtual string Name
+            {
+                get
+                {
+                    return "ipMatch";
+                }
+            }
+        }
+    }
+}
---
+++ casbinet/Util/Function/KeyMatch2Func.cs
@@ -0,0 +1,30 @@
+﻿namespace casbinet.util.function
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    //using AbstractFunction = com.googlecode.aviator.runtime.function.AbstractFunction;
+    //using FunctionUtils = com.googlecode.aviator.runtime.function.FunctionUtils;
+    //using AviatorBoolean = com.googlecode.aviator.runtime.type.AviatorBoolean;
+    //using AviatorObject = com.googlecode.aviator.runtime.type.AviatorObject;
+
+    public class KeyMatch2Func : AbstractFunction
+    {
+        public virtual AviatorObject call(IDictionary<string, object> env, AviatorObject arg1, AviatorObject arg2)
+        {
+            string key1 = FunctionUtils.getStringValue(arg1, env);
+            string key2 = FunctionUtils.getStringValue(arg2, env);
+
+            return AviatorBoolean.valueOf(BuiltInFunctions.keyMatch2(key1, key2));
+        }
+
+        public virtual string Name
+        {
+            get
+            {
+                return "keyMatch2";
+            }
+        }
+    }
+}
